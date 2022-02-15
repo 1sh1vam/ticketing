23cd19b9c49ffe5132585d0495cdb8eddeee83c3
1sh1vam <shivam07.mt@gmail.com>
2022-02-15 20:01:28 +0530

    adding the validation for email and password on request body

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,8 +1,18 @@
 import express from 'express';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
+router.get('/api/users/currentuser',[
+    body('email')
+        .isEmail()
+        .withMessage('Email is not valid'),
+    body('password')
+        .trim()
+        .isLength({ min: 4, max: 20 })
+        .withMessage('Password length must be between 4 and 20 charecters')
+], (req, res) => {
+    const { email, password } = req.body();
     res.send("Hi!, there.")
 });
 
