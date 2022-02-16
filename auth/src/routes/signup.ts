58f9aad64f2afe5e59c25a07f8c2c09b2270f19a
1sh1vam <shivam07.mt@gmail.com>
2022-02-16 22:51:50 +0530

    throwing the custom error with errror objct

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { RequestValidationError } from '../errors/request-validation-error';
 
 const router = express.Router();
 
@@ -17,10 +19,10 @@ router.post('/api/users/signup', [
     const errors = validationResult(req);
 
     if(!errors.isEmpty()) {
-        throw new Error('Email Id or Password is invalid');
+        throw new RequestValidationError(errors.array());
     }
 
-    throw new Error('Database connection failed');
+    throw new DatabaseConnectionError();
 
     res.send({});
 });
