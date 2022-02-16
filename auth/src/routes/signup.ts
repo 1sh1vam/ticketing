b1c777e5fc2df1cc85d2b7492b76d8ff1dda503e
1sh1vam <shivam07.mt@gmail.com>
2022-02-16 19:17:28 +0530

    added the validation error in signup

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,27 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
-    res.send('Hi! there.');
+router.post('/api/users/signup', [
+    body('email')
+        .isEmail()
+        .withMessage('Email is not valid'),
+    body('password')
+        .trim()
+        .isLength({ min: 4, max: 20 })
+        .withMessage('Password length must be between 4 and 20 charecters')
+], (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        return res.status(400).send(errors.array());
+    }
+    console.log('user is getting created');
+
+    res.send({});
 });
 
 export { router as signupRouter }
