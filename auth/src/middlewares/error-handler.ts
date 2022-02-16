108137645b731cd0bdaf46ade98ce749a673bab7
1sh1vam <shivam07.mt@gmail.com>
2022-02-16 22:53:01 +0530

    determining RequestValidationError and sendind the Request error

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+import { RequestValidationError } from "../errors/request-validation-error";
 
 export const errorHandler = (
   err: Error,
@@ -6,7 +7,17 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.log('Somethong went wrong');
+  if (err instanceof RequestValidationError) {
+      const errors = err.errors.map((error) => {
+          console.log('handling this error as request validation error');
+          return {
+              message: error.msg,
+              field: error.location,
+          }
+      });
+
+      res.status(400).send({ errors });
+  }
 
   res.status(400).send({
     message: 'Something went wrong',
