5fe2e72c2424e82c39de151d8facbcadd664efb4
1sh1vam <shivam07.mt@gmail.com>
2022-02-18 21:13:03 +0530

    extending the CustomError class to DatabaseConnectionError to validate the DatabaseConnectionError

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,14 +1,16 @@
-export class DatabaseConnectionError extends Error {
+import CustomError from "./custom-error";
+
+export class DatabaseConnectionError extends CustomError {
     statusCode = 500;
     reason = 'Error connecting to the database'
 
     constructor() {
-        super();
+        super('Error connecting to db');
     }
 
     serializeErrors() {
-        return {
+        return [{
             message: this.reason
-        }
+        }]
     }
 }
