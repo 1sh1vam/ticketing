b17d9ef2adb3dc2c94d3bbe42b61b422453cbc9c
1sh1vam <shivam07.mt@gmail.com>
2022-02-18 20:31:02 +0530

    adding the logic inside the RequestValidationError

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,7 +1,16 @@
 import { ValidationError } from "express-validator";
 
 export class RequestValidationError extends Error {
+    statusCode = 400;
+
     constructor(public errors: ValidationError[]) {
         super();
+    }
+
+    serializeErrors() {
+        return this.errors.map((err) => ({
+            messge: err.msg,
+            field: err.param,
+        }))
     }
 }
