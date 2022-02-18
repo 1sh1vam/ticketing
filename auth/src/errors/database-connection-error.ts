c7f130bd9b04aa478236891e51affa3c4645ee5d
1sh1vam <shivam07.mt@gmail.com>
2022-02-18 20:32:56 +0530

    adding the logic inside the DatabaseConnectionError

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,7 +1,14 @@
 export class DatabaseConnectionError extends Error {
+    statusCode = 500;
     reason = 'Error connecting to the database'
 
     constructor() {
         super();
+    }
+
+    serializeErrors() {
+        return {
+            message: this.reason
+        }
     }
 }
