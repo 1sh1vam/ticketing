0591652f3bfe0b7aa08b0610a41d8cf5f030a667
1sh1vam <shivam07.mt@gmail.com>
2022-02-22 20:30:41 +0530

    refactoring signup validation with  the middleware

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,9 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 import { BadRequestError } from '../errors/bad-request-error';
-import { RequestValidationError } from '../errors/request-validation-error';
 import { User } from '../models/user';
 import jwt from 'jsonwebtoken';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
@@ -15,39 +15,36 @@ router.post('/api/users/signup', [
         .trim()
         .isLength({ min: 4, max: 20 })
         .withMessage('Password length must be between 4 and 20 charecters')
-], async (req: Request, res: Response) => {
-    const errors = validationResult(req);
+    ],
+    validateRequest,
+    async (req: Request, res: Response) => {
 
-    if(!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
-    }
+        const { email, password } = req.body;
 
-    const { email, password } = req.body;
+        const existingUser = await User.findOne({ email });
 
-    const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            throw new BadRequestError('Email in use');
+        }
 
-    if (existingUser) {
-        throw new BadRequestError('Email in use');
-    }
+        const user = User.build({ email, password });
+        await user.save();
 
-    const user = User.build({ email, password });
-    await user.save();
+        // Generating a jwt token
 
-    // Generating a jwt token
+        const userJwt = jwt.sign({
+            id: user.id,
+            email: user.email,
+        }, process.env.JWT_KEY!); // asdf is a secret key which is used to generate the json token. This need to
+        // stored very securely. However for dev purposes I have used this simple string here.
+        // The exclamation mark after the process.env.JWT_KEY is to tell typescript that we are
+        // sure this key exists typescript doesn't need to worry about that.
 
-    const userJwt = jwt.sign({
-        id: user.id,
-        email: user.email,
-    }, process.env.JWT_KEY!); // asdf is a secret key which is used to generate the json token. This need to
-    // stored very securely. However for dev purposes I have used this simple string here.
-    // The exclamation mark after the process.env.JWT_KEY is to tell typescript that we are
-    // sure this key exists typescript doesn't need to worry about that.
+        req.session = {
+            jwt: userJwt
+        };
 
-    req.session = {
-        jwt: userJwt
-    };
-
-    res.status(201).send(user);
+        res.status(201).send(user);
 });
 
 export { router as signupRouter }
