9d20634d1cf65a17964082d7e9f26277e7f08a69
1sh1vam <shivam07.mt@gmail.com>
2022-02-27 15:52:50 +0530

    using currentUser middleware inside the currentUser route handler

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,21 +1,11 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', async (req, res) => {
-    // If token is not present directly send currentUser as null
-    if (!req.session?.jwt) {
-        return res.send({ currentUser: null });
-    };
-
-    // Now verifying the token
-    try {
-        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-        res.send({ currentUser: payload });
-    } catch (e) {
-        res.send({ currentUser: null });
-    }
+router.get('/api/users/currentuser', currentUser, async (req, res) => {
+    res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter }
