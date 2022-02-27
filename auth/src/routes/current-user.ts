5e399466138f96e5fd980d5a1fd52b21daadb38a
1sh1vam <shivam07.mt@gmail.com>
2022-02-27 16:04:07 +0530

    using the requireAuth middleware in current user endpoint

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,11 +1,16 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
 import { currentUser } from '../middlewares/current-user';
+import { requireAuth } from '../middlewares/require-auth';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', currentUser, async (req, res) => {
+router.get(
+  '/api/users/currentuser',
+  currentUser,
+  requireAuth,
+  async (req, res) => {
     res.send({ currentUser: req.currentUser || null });
-});
+  }
+);
 
 export { router as currentUserRouter }
