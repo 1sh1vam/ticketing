9feddbae301fa2e70b5f15b9f72dcf114a0c9bd7
1sh1vam <shivam07.mt@gmail.com>
2022-02-27 14:37:45 +0530

    completed the endpoint to get the current logged in user

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,21 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-    res.send("Hi!, there.")
+router.get('/api/users/currentuser', async (req, res) => {
+    // If token is not present directly send currentUser as null
+    if (!req.session?.jwt) {
+        return res.send({ currentUser: null });
+    };
+
+    // Now verifying the token
+    try {
+        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+        res.send({ currentUser: payload });
+    } catch (e) {
+        res.send({ currentUser: null });
+    }
 });
 
 export { router as currentUserRouter }
