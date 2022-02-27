e39aa6bf3e001f3aa1c098221b615972eb8ddbdb
1sh1vam <shivam07.mt@gmail.com>
2022-02-27 14:07:30 +0530

    completing the signin endpint and sending back the json token inside the cookie session

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+import { BadRequestError } from '../errors/bad-request-error';
 import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/user';
+import { Password } from '../services/password';
 
 const router = express.Router();
 
@@ -13,8 +17,34 @@ router.post('/api/users/signin', [
         .notEmpty()
         .withMessage('Provide a valid password')
     ], validateRequest,
-    (req: Request, res: Response) => {
+     async (req: Request, res: Response) => {
+        const { email, password } = req.body;
 
+        const existingUser = await User.findOne({ email });
+
+        if (!existingUser) {
+            throw new BadRequestError('Invalid credentials')
+        }
+
+        const passwordMatches = await Password.compare(existingUser.password, password);
+
+        if (!passwordMatches) {
+            throw new BadRequestError('Invalid credentials')
+        }
+
+        // Creating user jwt token
+        const userJwt = jwt.sign({
+            id: existingUser.id,
+            email: existingUser.email
+        }, process.env.JWT_KEY!);
+
+        // Setting the json token in cookie
+        req.session = {
+            jwt: userJwt
+        };
+
+
+        res.send(existingUser);
 });
 
 export { router as signinRouter }
