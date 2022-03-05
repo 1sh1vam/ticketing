da383c829c22f7cabc808d331f64e145d18094fa
1sh1vam <shivam07.mt@gmail.com>
2022-03-05 14:39:19 +0530

    refactoring the index.ts so as to be able to be test the app

---
+++ auth/src/app.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+import 'express-async-errors';
+import { NotFoundError } from './errors/not-found-error';
+import { errorHandler } from './middlewares/error-handler';
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import cookieSession from 'cookie-session';
+
+
+const app = express();
+app.set('trust proxy', true);
+
+app.use(express.json());
+app.use(cookieSession({
+    signed: false,
+    secure: true, // This option is to tell that set cookie only if connection is a secure connection.
+}))
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter)
+
+// Throwing an error if a request of any type (get, post etc.) is made to invalid route path
+app.all('*', async (req, res, next) => {
+    // when callback function is async it will throw unusual error like 502 bad gateway
+    // throw new NotFoundError()
+
+    // to handle this we can use next function by express
+    // next(new NotFoundError());
+
+    // another way we can handle this using the express-async-errors package by just importing it
+    throw new NotFoundError();
+})
+
+app.use(errorHandler);
+
+export { app };
