25b29ccdd04803bf3de26f472a97c5ec127d8b72
1sh1vam <shivam07.mt@gmail.com>
2022-03-06 13:42:47 +0530

    tests for the currentUser route handler

--- auth/src/routes/__test__/signout.test.ts
+++ auth/src/routes/__test__/signout.test.ts
@@ -9,7 +9,7 @@ it('returns a 401 if trying to signout', async () => {
 });
 
 it('clears the cookie after signing out', async () => {
-    const authRessponse = await request(app)
+    const authResponse = await request(app)
         .post('/api/users/signup')
         .send({
             email: 'test@test.com',
@@ -17,15 +17,14 @@ it('clears the cookie after signing out', async () => {
         })
         .expect(201);
 
-    const cookie = authRessponse.get('Set-Cookie');
+    const cookie = authResponse.get('Set-Cookie');
 
     const response = await request(app)
         .post('/api/users/signout')
-        .set('Cookie', cookie)
+        .set('Cookie', cookie) // setting the cookie that we get form the signup
         .send({})
         .expect(200);
 
     expect(response.get('Set-Cookie')[0]).toEqual('session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly')
-    console.log('chek', response.get('Set-Cookie'))
 });
 
