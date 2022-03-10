6a569b8466024ace24987ddeb7129b956b5db71e
1sh1vam <shivam07.mt@gmail.com>
2022-03-10 19:32:07 +0530

    checking if the axios call is moved to the component then the page loads or not

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,15 +2,20 @@ import axios from "axios";
 
 const LandingPage = ({ currentUser }) => {
     console.log('inside the component', currentUser);
+    axios.get('/api/users/currentUser').catch((err) => {
+        // We were getting the ECONNREFUSED 127.0.0.1:80 in console so handling this error here
+        console.log(err.message)
+    });
+
     return <h1>Landing page...</h1>
 }
 
-LandingPage.getInitialProps = async () => {
+// LandingPage.getInitialProps = async () => {
     // We are not using the useRequest hook here as hooks can only be used inside a
     // component and this is not a component.
-    const response = await axios.get('/api/users/currentUser');
+    // const response = await axios.get('/api/users/currentUser');
 
-    return response.data;
-}
+    // return response.data;
+// }
 
 export default LandingPage;
