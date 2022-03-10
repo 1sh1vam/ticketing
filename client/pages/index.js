6c2c6017d0262bbf205818816a00d87ff4ce0343
1sh1vam <shivam07.mt@gmail.com>
2022-03-10 19:21:22 +0530

    using the getInitialProps inorder to make initial call to get the data and send the content in html file with the content to the client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,12 @@
-export default () => {
+const LandingPage = ({ color }) => {
+    console.log('inside the component', color);
     return <h1>Landing page...</h1>
 }
+
+LandingPage.getInitialProps = () => {
+    console.log('inside the server');
+
+    return { color: 'red' }
+}
+
+export default LandingPage;
