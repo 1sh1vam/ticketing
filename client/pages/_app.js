815030189ede9fc2e8b52911ee4f91ef6abd729b
1sh1vam <shivam07.mt@gmail.com>
2022-03-13 15:39:14 +0530

    using the getInitialProps in custom app component to get the currentUser so as to be able to pass the currentUser in other components like Header through props

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,27 @@
 import '../styles/globals.css';
+import buildClient from './api/build-client';
 
-export default ({ Component, pageProps }) => {
-    return <Component {...pageProps} />
+const AppComponent =  ({ Component, pageProps }) => {
+    return (
+        <div>
+            {/* We have a problem here as we need to change header content based on the
+            idea if the user is logged in or not. But we are making the currentUser get call
+            inside the landinPAge component. We will user AppComponent to get the users here and
+            pass it through the props */}
+            <h1>Header COmponen</h1>
+            <Component {...pageProps} />
+        </div>
+    )
 }
+
+AppComponent.getInitialProps = (appContext) => {
+    console.log('App initial props', Object.keys(appContext))
+    // The context inside the custom app component is different from the context inside other
+    // components. Here we have an extra property Component. appContext.ctx is the context that
+    // is having the req object.
+    const client = buildClient(appContext.ctx);
+    const { data } = await client.get('/api/users/currentUser');
+    return data
+}
+
+export default AppComponent;
--- client/pages/index.js
+++ client/pages/index.js
@@ -10,6 +10,7 @@ const LandingPage = ({ currentUser }) => {
 }
 
 LandingPage.getInitialProps = async (context) => {
+    console.log('INISDE THE LANDING PAGE');
     const client = buildClient(context);
     const { data } = await client.get('/api/users/currentUser');
     return data;
