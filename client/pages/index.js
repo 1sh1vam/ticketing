404287920bec4197a36c7d6a72ec6aecfe782bb1
1sh1vam <shivam07.mt@gmail.com>
2022-03-13 17:38:47 +0530

    showing the message as per signed in or not

--- client/pages/index.js
+++ client/pages/index.js
@@ -6,7 +6,7 @@ const LandingPage = ({ currentUser }) => {
     //     console.log(err.message)
     // });
 
-    return <h1>Landing page...</h1>
+    return <h1>{currentUser ? 'You are signed in' : 'You are not signed in'}</h1>
 }
 
 LandingPage.getInitialProps = async (context) => {
