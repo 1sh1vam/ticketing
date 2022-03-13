03caeebb14fe1181a1eab2a5f19ee7d97c85c97d
1sh1vam <shivam07.mt@gmail.com>
2022-03-13 15:26:38 +0530

    replacing the browser/server check and the request with the reusable buildClient function

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,39 +1,18 @@
-import axios from "axios";
-
+import buildClient from "./api/build-client";
 const LandingPage = ({ currentUser }) => {
     console.log('inside the component', currentUser);
-    axios.get('/api/users/currentUser').catch((err) => {
-        // We were getting the ECONNREFUSED 127.0.0.1:80 in console so handling this error here
-        console.log(err.message)
-    });
+    // axios.get('/api/users/currentUser').catch((err) => {
+    //     // We were getting the ECONNREFUSED 127.0.0.1:80 in console so handling this error here
+    //     console.log(err.message)
+    // });
 
     return <h1>Landing page...</h1>
 }
 
-LandingPage.getInitialProps = async ({ req }) => {
-    // We are not using the useRequest hook here as hooks can only be used inside a
-    // component and this is not a component.
-
-    // getInitialProps can also be called from the client when we are routing from the
-    // application itself.
-
-    if (typeof window === 'undefined') {
-        // If window is undefined then we are inside the server not in the client
-
-        // "ingress-nginx-controller" is the service name of ingress service inside the ingress-nginx namespace.
-        // "ingress-nginx" is the namespace
-        const { data } = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentUser', {
-            headers: req.headers,
-            // Sending the whole header coming from the req obect similar to the express req.
-        });
-        return data
-
-    } else {
-        const { data } = await axios.get('/api/users/currentUser');
-        return data;
-    }
-
-    return {}
+LandingPage.getInitialProps = async (context) => {
+    const client = buildClient(context);
+    const { data } = await client.get('/api/users/currentUser');
+    return data;
 }
 
 export default LandingPage;
