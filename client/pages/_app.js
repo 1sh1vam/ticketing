f39bcbfe987d4241d32b6a9b58566cd316ed31a2
1sh1vam <shivam07.mt@gmail.com>
2022-03-13 17:04:29 +0530

    passing the pageProps and currentUser

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,21 +1,20 @@
 import '../styles/globals.css';
 import buildClient from './api/build-client';
 
-const AppComponent =  ({ Component, pageProps }) => {
+const AppComponent =  ({ Component, pageProps, currentUser }) => {
     return (
         <div>
             {/* We have a problem here as we need to change header content based on the
             idea if the user is logged in or not. But we are making the currentUser get call
             inside the landinPAge component. We will user AppComponent to get the users here and
             pass it through the props */}
-            <h1>Header COmponen</h1>
+            <h1>Header COmponen {currentUser.email}</h1>
             <Component {...pageProps} />
         </div>
     )
 }
 
 AppComponent.getInitialProps = async (appContext) => {
-    console.log('app context', Object.keys(appContext))
     const client = buildClient(appContext.ctx);
     const { data } = await client.get('/api/users/currentUser');
 
@@ -23,9 +22,17 @@ AppComponent.getInitialProps = async (appContext) => {
     // we can see that getInitialProps in other components never gets triggered.
     // To handle multiple getInitialProps we have to call components getInitialProps
     // here like below.
-    const pageProps = await appContext.Component.getInitialProps(appContext.ctx)
-    console.log('page props', pageProps);
-    return data
+
+    let pageProps = {};
+    // There might be few components which does not have any getInitialProps at all.
+    if (appContext.Component.getInitialProps) {
+        pageProps = await appContext.Component.getInitialProps(appContext.ctx)
+    }
+
+    return {
+        pageProps,
+        ...data,
+    };
 }
 
 export default AppComponent;
