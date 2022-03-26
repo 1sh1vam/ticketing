6bfed6df7e8b7fd7e0e8eda5bb05f229bc0f9087
1sh1vam <shivam07.mt@gmail.com>
2022-03-26 21:28:00 +0530

    debuggin unexpect 400 error while expecting the 404, because of the non hex id sending to the mongodb

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,9 +1,11 @@
 import request from 'supertest';
 import { app } from '../../app';
+import mongoose from 'mongoose';
 
 it('returns a 404 error when ticket is not found', async () => {
+    const id = new mongoose.Types.ObjectId().toHexString();
     await request(app)
-        .get('/api/tickets/in2138n213u')
+        .get(`/api/tickets/${id}`)
         .send()
         .expect(404)
 });
