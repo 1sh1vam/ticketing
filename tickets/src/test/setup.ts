ba13d39e5e2912216a9dd06e9f14d4bfcb9d313c
1sh1vam <shivam07.mt@gmail.com>
2022-03-26 16:22:44 +0530

    generating the cookie to simulate we are signed in into tests

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
 import { MongoMemoryServer } from "mongodb-memory-server";
-import { app } from "../app";
+import jwt from 'jsonwebtoken';
+
+declare global {
+    var signin: () => string[];
+}
 
 let mongo: any;
 
 beforeAll(async () => {
+    process.env.JWT_KEY = 'jewrwer'
     mongo = await MongoMemoryServer.create();
     const mongoUri = mongo.getUri();
     await mongoose.connect(mongoUri);
@@ -22,3 +27,23 @@ afterAll(async () => {
     await mongo.stop();
     await mongoose.connection.close();
 });
+
+global.signin = () => {
+    // Step: 1 create a payload for jwt
+    const payload = { id: '12njsb1', email: 'somerandom@test.com' };
+
+    // Step: 2 create a jwt token
+    const token = jwt.sign(payload, process.env.JWT_KEY!)
+
+    // Step 3: Creating session object { jwt: MY_JWT }
+    const session = { jwt: token };
+
+    // Step 4: Turn session into a json
+    const sessionJson = JSON.stringify(session);
+
+    // Step 5: Encoding Json into a base64 string
+    const base64 = Buffer.from(sessionJson).toString('base64');
+
+    // return a string thats the cookie with encoded data
+    return [`express:sess-${base64}`]
+}
