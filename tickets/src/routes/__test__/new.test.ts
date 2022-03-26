e42a7289608c141c60068130f7821bf197d503a2
1sh1vam <shivam07.mt@gmail.com>
2022-03-26 16:07:16 +0530

    test for not able to access the route handler of not sined in

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -10,7 +10,10 @@ it('has a route handler listening for /api/tickets post request', async () => {
 });
 
 it('can only be accessed if user is signed in', async () => {
-
+    return request(app)
+        .post('/api/tickets')
+        .send({})
+        .expect(401);
 });
 
 it('returns a status code other than 401 if user is signed in', async () => {
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from 'express';
 const router = express.Router();
 
 router.post('/api/tickets', async (req, res) => {
-    return res.sendStatus(401);
+    return res.sendStatus(405);
 });
 
 
