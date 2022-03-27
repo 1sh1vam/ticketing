be711a0af747bb154ccc9732819f58b856d25d31
1sh1vam <shivam07.mt@gmail.com>
2022-03-27 15:29:07 +0530

    adding the route handler to list all the tickets

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,6 +4,7 @@ import cookieSession from 'cookie-session';
 import { currentUser, errorHandler, NotFoundError } from '@simtix/ticketing-common';
 import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show';
+import { listTicketsRouter } from './routes';
 
 const app = express();
 app.set('trust proxy', true);
@@ -19,6 +20,7 @@ app.use(currentUser);
 
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(listTicketsRouter);
 
 app.all('*',async () => {
     throw new NotFoundError();
