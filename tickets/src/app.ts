17d3c4e2019427637ec30a277246d0617871b9df
1sh1vam <shivam07.mt@gmail.com>
2022-03-27 15:43:53 +0530

    throwing an error if the ticket is not present in the db in /api/tickets/:id put call

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,6 +5,7 @@ import { currentUser, errorHandler, NotFoundError } from '@simtix/ticketing-comm
 import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show';
 import { listTicketsRouter } from './routes';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
 app.set('trust proxy', true);
@@ -21,6 +22,7 @@ app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
 app.use(listTicketsRouter);
+app.use(updateTicketRouter);
 
 app.all('*',async () => {
     throw new NotFoundError();
