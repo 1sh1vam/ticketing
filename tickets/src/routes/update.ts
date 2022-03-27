d9be86a9e444f570f44e8468310722ec23c2bd92
1sh1vam <shivam07.mt@gmail.com>
2022-03-27 16:09:11 +0530

    validating the update request before updating the ticket

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -1,20 +1,31 @@
-import { NotAuthorizedError, NotFoundError, requireAuth } from '@simtix/ticketing-common';
+import { NotAuthorizedError, NotFoundError, requireAuth, validateRequest } from '@simtix/ticketing-common';
 import express, {Request, Response} from 'express';
 import { body } from 'express-validator';
 import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
-router.put('/api/tickets/:id', requireAuth, async (req: Request, res: Response) => {
+router.put(
+  '/api/tickets/:id',
+  requireAuth,
+  [
+    body('title').notEmpty().withMessage('title must be defined'),
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('price must be greater than 0'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
     const ticket = await Ticket.findById(req.params.id);
 
-    if (!ticket) throw new NotFoundError
+    if (!ticket) throw new NotFoundError();
 
     if (ticket.userId !== req.currentUser!.id) {
-        throw new NotAuthorizedError();
+      throw new NotAuthorizedError();
     }
 
     res.send(ticket);
-});
+  }
+);
 
 export { router as updateTicketRouter }
