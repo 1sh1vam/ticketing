8c632adaba122639c9daa39115e7a84e2ae0ab21
1sh1vam <shivam07.mt@gmail.com>
2022-03-27 15:55:58 +0530

    testing if the user can modify tickets of other users

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -26,7 +26,29 @@ it('returns a 401 error if not logged in', async () => {
 });
 
 it('returns a 400 bad request error if sent invalid parameters', async () => {
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'asf',
+            price: 23
+        });
 
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set('Cookie', global.signin())
+        .send({
+            title: 'new title',
+            price: 100
+        })
+        .expect(401);
+
+    const newResponse = await request(app)
+        .get(`/api/tickets/${response.body.id}`)
+        .send();
+
+    expect(newResponse.body.title).toEqual(response.body.title);
+    expect(newResponse.body.price).toEqual(response.body.price);
 });
 
 it('returns a 401 if trying to modify a ticket which is not owned by the user', async () => {
