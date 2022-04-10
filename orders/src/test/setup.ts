1de6461226b2a78317b3d43d9f461262da33cc5b
1sh1vam <shivam07.mt@gmail.com>
2022-04-10 15:57:53 +0530

    setting up the tests for the orders service

---
+++ orders/src/test/setup.ts
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import jwt from 'jsonwebtoken';
+
+declare global {
+    var signin: () => string[];
+}
+
+let mongo: any;
+beforeAll(async () => {
+    process.env.JWT_KEY = 'asfr';
+    mongo = await MongoMemoryServer.create();
+    const uri = mongo.getUri();
+    await mongoose.connect(uri);
+});
+
+beforeEach(async () => {
+    const collections = await mongoose.connection.db.collections();
+
+    for(const collection of collections) {
+        await collection.deleteMany({});
+    }
+});
+
+afterAll(async () => {
+    await mongo.stop();
+    await mongoose.connection.close();
+});
+
+global.signin = () => {
+    const id = new mongoose.Types.ObjectId().toHexString();
+    const payload = { id, email: 'test@test.com' };
+
+    const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+    const session = { jwt: token };
+
+    const sessionJson = JSON.stringify(session);
+
+    const base64 = Buffer.from(sessionJson).toString('base64');
+
+    return [`sess=${base64}`]
+}
