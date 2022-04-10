88a05f875c31e8c5061af0826d10c2f0d3244739
1sh1vam <shivam07.mt@gmail.com>
2022-04-10 16:55:09 +0530

    testing order service with invalid ticketId

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -7,3 +7,18 @@ it('returns a 401 unauthorized error if trying to create an order without being 
         .send({ ticketId: '23dre234' })
         .expect(401);
 });
+
+it('returns bad request error if user sends create order with invalid ticket id', async () =>{
+    console.log(global.signin());
+    await request(app)
+        .post('/api/orders')
+        .set('Cookie', global.signin())
+        .send({})
+        .expect(400);
+
+    await request(app)
+        .post('/api/orders')
+        .set('Cookie', global.signin())
+        .send({ ticketId: '712364' })
+        .expect(400);
+});
