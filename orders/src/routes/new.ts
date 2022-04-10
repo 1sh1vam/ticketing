4a6ae3e7ad169fdf26edeb60549e607097aca07b
1sh1vam <shivam07.mt@gmail.com>
2022-04-10 15:01:29 +0530

    creating the order in db and sending the response

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -32,6 +32,20 @@ router.post(
     // 3. Calculate an expiration date for this order.
     const expiration = new Date();
     expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    // 4. Create the order and save it to the database
+    const order = Order.build({
+        userId: req.currentUser!.id,
+        status: OrderStatus.Created,
+        expiresAt: expiration,
+        ticket,
+    });
+
+    await order.save();
+
+    // 5. Publish an event saying that an order was creared
+
+    res.status(201).send(order);
   }
 );
 
