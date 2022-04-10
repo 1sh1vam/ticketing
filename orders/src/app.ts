9fef89483e7054af72f2a63b69bd27b752925ee7
1sh1vam <shivam07.mt@gmail.com>
2022-04-10 12:48:09 +0530

    setting up the route for creating new order

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@simtix/ticketing-common';
+import { currentUser, errorHandler, NotFoundError } from '@simtix/ticketing-common';
+import { newOrderRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -11,6 +12,10 @@ app.use(cookieSession({
     signed: false,
     secure: process.env.NODE_ENV !== 'test',
 }));
+
+app.use(currentUser);
+
+app.use(newOrderRouter);
 
 app.use('*', () => {
     throw new NotFoundError();
