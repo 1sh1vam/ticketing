dcc0662c972072be373dfe9ec143332e60d4fe1f
1sh1vam <shivam07.mt@gmail.com>
2022-04-13 21:36:20 +0530

    test suits for the showOrder route

---
+++ orders/src/routes/__test__/show.test.ts
@@ -0,0 +1,64 @@
+import mongoose from 'mongoose';
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+
+it('throws not found error if ticket does not exists', async () => {
+    const orderId = new mongoose.Types.ObjectId().toHexString();
+
+    await request(app)
+        .get(`/api/orders/${orderId}`)
+        .set('Cookie', global.signin())
+        .send()
+        .expect(404)
+});
+
+it('throws unauthorized error if accessing order of other users', async () => {
+    const ticket = Ticket.build({
+        title: 'KXIP vs MI',
+        price: 3498,
+    });
+
+    await ticket.save();
+
+    const { body: order} = await request(app)
+        .post('/api/orders')
+        .set('Cookie', global.signin())
+        .send({
+            ticketId: ticket.id,
+        })
+        .expect(201);
+
+    await request(app)
+        .get(`/api/orders/${order.id}`)
+        .set('Cookie', global.signin())
+        .send()
+        .expect(401);
+});
+
+it('throws unauthorized error if accessing order of other users', async () => {
+    const ticket = Ticket.build({
+        title: 'KXIP vs MI',
+        price: 3498,
+    });
+
+    await ticket.save();
+
+    const user = global.signin();
+
+    const { body: order} = await request(app)
+        .post('/api/orders')
+        .set('Cookie', user)
+        .send({
+            ticketId: ticket.id,
+        })
+        .expect(201);
+
+    const { body: fetchedOrder } =  await request(app)
+        .get(`/api/orders/${order.id}`)
+        .set('Cookie', user)
+        .send()
+        .expect(200);
+
+    expect(fetchedOrder.id).toEqual(order.id);
+});
