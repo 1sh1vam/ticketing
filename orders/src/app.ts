93288127078ff8ace415d32c7b637dfa265c3e0e
1sh1vam <shivam07.mt@gmail.com>
2022-04-13 21:35:58 +0530

    hooking the showOrderRouter in app

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -4,6 +4,7 @@ import cookieSession from 'cookie-session';
 import { currentUser, errorHandler, NotFoundError } from '@simtix/ticketing-common';
 import { newOrderRouter } from './routes/new';
 import { listOrdersRouter } from './routes';
+import { showOrderRouter } from './routes/show';
 
 const app = express();
 app.set('trust proxy', true);
@@ -19,6 +20,7 @@ app.use(currentUser);
 
 app.use(newOrderRouter);
 app.use(listOrdersRouter);
+app.use(showOrderRouter);
 
 app.all('*', async () => {
     throw new NotFoundError();
