747c561ab8ab8c6954739ba8f9f8cd067ad87722
1sh1vam <shivam07.mt@gmail.com>
2022-04-16 16:01:45 +0530

    testing the delete order event

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('throws not found error if trying do delete which does not exists', async () => {
     const orderId = new mongoose.Types.ObjectId().toHexString();
@@ -67,3 +68,29 @@ it('deletes the order', async () => {
 
     expect(fetchedOrder.status).toEqual(OrderStatus.Cancelled);
 });
+
+it('publishes order cancelled event', async () => {
+    const ticket = Ticket.build({
+        title: 'KKR vs DC',
+        price: 234
+    });
+    await ticket.save();
+
+    const user = global.signin();
+
+    const { body: order } = await request(app)
+        .post('/api/orders')
+        .set('Cookie', user)
+        .send({
+            ticketId: ticket.id,
+        })
+        .expect(201);
+
+    await request(app)
+        .delete(`/api/orders/${order.id}`)
+        .set('Cookie', user)
+        .send()
+        .expect(204)
+
+    expect(natsWrapper.client.publish).toHaveBeenCalled()
+})
