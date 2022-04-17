8abb0c551f4d6dcf30c62246dab9bfd2885bf4d4
1sh1vam <shivam07.mt@gmail.com>
2022-04-17 15:00:49 +0530

    testing if version number is incremented as expected

--- tickets/src/models/__test__/ticket.test.ts
+++ tickets/src/models/__test__/ticket.test.ts
@@ -24,3 +24,18 @@ it('implements optimistic concurrency control', async () => {
 
     throw new Error('Should not reach this point');
 });
+
+it('increments version number on multiple saves', async () => {
+    const ticket = Ticket.build({
+        title: 'CSK vs GT',
+        price: 234,
+        userId: '3144'
+    });
+
+    await ticket.save();
+    expect(ticket.version).toEqual(0);
+    await ticket.save();
+    expect(ticket.version).toEqual(1);
+    await ticket.save();
+    expect(ticket.version).toEqual(2);
+});
