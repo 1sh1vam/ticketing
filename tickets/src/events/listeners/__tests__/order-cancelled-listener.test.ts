f3d0f8f96a4b3a7c51f825b0ea0486130020d4fb
1sh1vam <shivam07.mt@gmail.com>
2022-04-22 20:35:47 +0530

    OrderCancelledEvent test setup

---
+++ tickets/src/events/listeners/__tests__/order-cancelled-listener.test.ts
@@ -0,0 +1,35 @@
+import { OrderCancelledEvent } from "@simtix/ticketing-common";
+import mongoose from "mongoose";
+import { Message } from "node-nats-streaming";
+import { Ticket } from "../../../models/ticket";
+import { natsWrapper } from "../../../nats-wrapper"
+import { OrderCancelledListener } from "../order-cancelled-listener"
+
+const setup = async () => {
+    const listener = new OrderCancelledListener(natsWrapper.client);
+
+    const orderId = new mongoose.Types.ObjectId().toHexString();
+
+    const ticket = Ticket.build({
+        title: 'RR vs DC',
+        price: 2123,
+        userId: 'dfdg',
+    });
+    ticket.set({ orderId });
+    await ticket.save();
+
+    const data: OrderCancelledEvent['data'] = {
+        id: orderId,
+        version: 0,
+        ticket: {
+            id: ticket.id,
+        },
+    };
+
+    // @ts-ignore
+    const msg: Message = {
+        ack: jest.fn(),
+    }
+
+    return { listener, ticket, orderId, data, msg };
+};
