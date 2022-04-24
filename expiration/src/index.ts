c1999f137e6e736926173125ff1adf8ce6405c00
1sh1vam <shivam07.mt@gmail.com>
2022-04-24 16:57:03 +0530

    listening to the OrderCreatedListener in expiration service

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,3 +1,4 @@
+import { OrderCreatedListener } from "./events/listener/order-created-listener"
 import { natsWrapper } from "./nats-wrapper"
 
 const start = async () => {
@@ -10,6 +11,9 @@ const start = async () => {
     if (!process.env.NATS_URL) {
         throw new Error('NATS_URL is required')
     }
+    if (!process.env.REDIS_HOST) {
+        throw new Error('REDIS_HOST is required')
+    }
 
     try {
         await natsWrapper.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL);
@@ -19,9 +23,12 @@ const start = async () => {
 
         process.on('SIGINT', () => natsWrapper.client.close());
         process.on('SIGTERM', () => natsWrapper.client.close());
+
+        new OrderCreatedListener(natsWrapper.client).listen();
     } catch (err) {
         console.log('Error connecting to nats in expiration service');
     }
+    console.log('expiration service');
 }
 
 start();
