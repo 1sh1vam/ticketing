6482029afe55541a87c823a9bdc723fd64ae79d1
1sh1vam <shivam07.mt@gmail.com>
2022-04-24 12:42:00 +0530

    locking the reserved ticket from editing

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -1,4 +1,4 @@
-import { NotAuthorizedError, NotFoundError, requireAuth, validateRequest } from '@simtix/ticketing-common';
+import { BadRequestError, NotAuthorizedError, NotFoundError, requireAuth, validateRequest } from '@simtix/ticketing-common';
 import express, {Request, Response} from 'express';
 import { body } from 'express-validator';
 import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
@@ -21,6 +21,10 @@ router.put(
     const ticket = await Ticket.findById(req.params.id);
 
     if (!ticket) throw new NotFoundError();
+
+    if (ticket.orderId) {
+      throw new BadRequestError('Can not edit a reserved ticket');
+    }
 
     if (ticket.userId !== req.currentUser!.id) {
       throw new NotAuthorizedError();
