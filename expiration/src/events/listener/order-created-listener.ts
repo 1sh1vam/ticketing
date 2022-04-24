fff9e2b17591d059e2594d8e0876eb035a5c4c53
1sh1vam <shivam07.mt@gmail.com>
2022-04-24 16:58:04 +0530

    adding the new order created event to the expiration queue

--- expiration/src/events/listener/order-created-listener.ts
+++ expiration/src/events/listener/order-created-listener.ts
@@ -1,5 +1,6 @@
 import { Listener, OrderCreatedEvent, OrderStatus, Subjects } from "@simtix/ticketing-common";
 import { Message } from "node-nats-streaming";
+import { expirationQueue } from "../../queues/expiration-queue";
 import { queueGroupName } from "./queue-group-name";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
@@ -8,6 +9,10 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     queueGroupName = queueGroupName;
 
     async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+        expirationQueue.add({
+            orderId: data.id,
+        });
 
+        msg.ack();
     }
 }
