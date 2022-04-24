ac110949accbb37e726948dccaabc671afd8f121
1sh1vam <shivam07.mt@gmail.com>
2022-04-24 18:21:57 +0530

    adding delay to wait before processing the queue job

--- expiration/src/events/listener/order-created-listener.ts
+++ expiration/src/events/listener/order-created-listener.ts
@@ -11,6 +11,8 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
         expirationQueue.add({
             orderId: data.id,
+        }, {
+            delay: 10000,
         });
 
         msg.ack();
