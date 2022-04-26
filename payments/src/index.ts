dfe957cb5e81faf68decac7bd3172ee7e05fda09
1sh1vam <shivam07.mt@gmail.com>
2022-04-26 21:07:32 +0530

    setting up index.ts for the payments service

---
+++ payments/src/index.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const start = async () => {
+    if (!process.env.JWT_KEY) {
+        throw new Error('JWT_KEY is required');
+    }
+    if (!process.env.MONGO_URI) {
+        throw new Error('MONGO_URI is required');
+    }
+    if (!process.env.NATS_CLUSTER_ID) {
+        throw new Error('NATS_CLUSTER_ID is required');
+    }
+    if (!process.env.NATS_CLIENT_ID) {
+        throw new Error('NATS_CLIENT_ID is required');
+    }
+    if (!process.env.NATS_URL) {
+        throw new Error('NATS_URL is required');
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+    } catch(err) {
+        console.log('failed to connect to the database', err);
+    }
+}
