15e1b2204d36aafef5678a800ec2cb892c07260a
1sh1vam <shivam07.mt@gmail.com>
2022-05-01 15:50:21 +0530

    setting up the create charge route

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,12 +1,32 @@
+import { BadRequestError, NotAuthorizedError, NotFoundError, OrderStatus, requireAuth, validateRequest } from '@simtix/ticketing-common';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
 router.post(
   '/api/payments',
+  requireAuth,
   [body('orderId').not().isEmpty(), body('token').not().isEmpty()],
+  validateRequest,
   async (req: Request, res: Response) => {
+    const { orderId, token } = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+        throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+        throw new NotAuthorizedError();
+    }
+
+    if (order.status === OrderStatus.Cancelled) {
+        throw new BadRequestError('Order is cancelled');
+    }
+
     res.send({ success: true });
   }
 );
