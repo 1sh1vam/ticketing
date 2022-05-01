2a20ab86e35c2a50ea2bd2a997475c028b8acec7
1sh1vam <shivam07.mt@gmail.com>
2022-05-01 16:06:45 +0530

    testing bad request if creating the payment for an order which is cancelled

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -34,3 +34,25 @@ it('throws a 401 if user is not the one who created order', async () => {
         })
         .expect(401);
 });
+
+it('throws 400 if order is cancelled', async () => {
+    const userId = new mongoose.Types.ObjectId().toHexString();
+
+    const order = Order.build({
+        id: new mongoose.Types.ObjectId().toHexString(),
+        price: 30,
+        version: 0,
+        userId,
+        status: OrderStatus.Cancelled,
+    });
+    await order.save();
+
+    await request(app)
+        .post('/api/payments')
+        .set('Cookie', global.signin(userId))
+        .send({
+            orderId: order.id,
+            token: 'dfdfgdfg',
+        })
+        .expect(400);
+});
