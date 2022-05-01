b770e78a8bcaa7b4bfc700ff4bd463e3e5f5d7ac
1sh1vam <shivam07.mt@gmail.com>
2022-05-01 16:01:18 +0530

    testing the unauthorized edit of payment testing user can not do payment for an order not created by him

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -1,6 +1,8 @@
+import { OrderStatus } from '@simtix/ticketing-common';
 import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../../app';
+import { Order } from '../../models/order';
 
 it('throws 404 if orderId does not exists', async () => {
     await request(app)
@@ -13,3 +15,24 @@ it('throws 404 if orderId does not exists', async () => {
         .expect(404);
 });
 
+it('throws a 401 if user is not the one who created order', async () => {
+    const order = Order.build({
+        id: new mongoose.Types.ObjectId().toHexString(),
+        price: 20,
+        version: 0,
+        userId: 'derewr',
+        status: OrderStatus.Created,
+    });
+    await order.save();
+
+    await request(app)
+        .post('/api/payments')
+        .set('Cookie', global.signin())
+        .send({
+            orderId: order.id,
+            token: 'sdfsdg',
+        })
+        .expect(401);
+});
+
+it()
