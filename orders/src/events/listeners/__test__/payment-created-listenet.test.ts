05dcbc173249b8d43500fb135b97a31950785547
1sh1vam <shivam07.mt@gmail.com>
2022-05-06 21:20:09 +0530

    payment created test

---
+++ orders/src/events/listeners/__test__/payment-created-listenet.test.ts
@@ -0,0 +1,46 @@
+import { Order } from "../../../models/order";
+import { Ticket } from '../../../models/ticket';
+import { natsWrapper } from "../../../nats-wrapper"
+import { PaymentCreatedListener } from "../payment-created-listener"
+import { OrderStatus, PaymentCreatedEvent } from "@simtix/ticketing-common";
+import mongoose from "mongoose";
+import { Message } from "node-nats-streaming";
+
+const setup = async () => {
+    const listener = new PaymentCreatedListener(natsWrapper.client);
+
+    const ticket = Ticket.build({
+        title: 'new match',
+        price: 230,
+        id: new mongoose.Types.ObjectId().toHexString(),
+    });
+    await ticket.save();
+
+    const order = Order.build({
+        userId: 'dsfjt43jfg',
+        status: OrderStatus.Created,
+        expiresAt: new Date(),
+        ticket,
+    });
+    await order.save();
+
+    const data: PaymentCreatedEvent['data'] = {
+        id: new mongoose.Types.ObjectId().toHexString(),
+        orderId: order.id,
+        stripeId: 'dsffgfg',
+    }
+
+    // @ts-ignore
+    const msg: Message = {
+        ack: jest.fn(),
+    }
+
+    return {
+        listener,
+        ticket,
+        order,
+        data,
+        msg
+    }
+}
+
