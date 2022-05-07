3a89154f80840e06606aa4d048216eb28be2b83d
1sh1vam <shivam07.mt@gmail.com>
2022-05-07 15:47:52 +0530

    displaying list of tickets

--- client/components/ticket-card.js
+++ client/components/ticket-card.js
@@ -1,8 +1,10 @@
 const TicketCard = ({ title, price }) => {
     return (
-        <div className="w-full p-2 flex flex-col text-md font-md shadow-lg bg-gray-150 text-gray-800 gap-5">
+        <div className="w-full p-5 flex items-start rounded-md justify-between text-md font-md shadow-lg bg-gray-100 text-gray-800 gap-5">
             <p className="m-0">{title}</p>
-            <p className="m-0">{title}</p>
+            <p className="m-0">${price}</p>
         </div>
     )
 }
+
+export default TicketCard;
