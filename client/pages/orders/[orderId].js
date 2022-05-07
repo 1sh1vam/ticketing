7610f5709aec265fe59ea5e66ba87a0fde2724a5
1sh1vam <shivam07.mt@gmail.com>
2022-05-07 18:36:36 +0530

    paying for an order

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,9 +1,18 @@
 import { useEffect, useState } from "react";
 import StripeCheckOut from 'react-stripe-checkout';
 import { STRIPE_KEY } from '../../constants/stripe-key';
+import useRequest from "../../hooks/use-request";
 
 const OrderShow = ({ order }) => {
     const [timeLeft, setTimeLeft] = useState(0);
+    const { sendRequest, errors } = useRequest({
+        url: '/api/payments',
+        method: 'post',
+        body: {
+            orderId: order.id,
+        },
+        onSuccess: (payment) => console.log('payments', payment),
+    });
 
     useEffect(() => {
         const findTimeLeft = () => {
@@ -25,9 +34,14 @@ const OrderShow = ({ order }) => {
         <div className="flex flex-col gap-5">
             <p>Time left: {timeLeft}</p>
             <StripeCheckOut
-                token={(token) => console.log('token', token)}
+                token={(token) => sendRequest({ token: token.id })}
                 stripeKey={STRIPE_KEY}
             />
+            {errors?.generic && (
+                <ul className="bg-red-400 mt-2 list-disc text-white rounded-md pl-8 pr-5 py-3">
+                    {errors.generic}
+                </ul>
+            )}
         </div>
     )
 }
