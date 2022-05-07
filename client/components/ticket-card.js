7976dcb6a2536849690a0fe92bc62f9b2630bb31
1sh1vam <shivam07.mt@gmail.com>
2022-05-07 14:58:11 +0530

    displaying ticket

---
+++ client/components/ticket-card.js
@@ -0,0 +1,8 @@
+const TicketCard = ({ title, price }) => {
+    return (
+        <div className="w-full p-2 flex flex-col text-md font-md shadow-lg bg-gray-150 text-gray-800 gap-5">
+            <p className="m-0">{title}</p>
+            <p className="m-0">{title}</p>
+        </div>
+    )
+}
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,13 @@
 import buildClient from "../api/build-client";
 const LandingPage = ({ currentUser, tickets }) => {
+    const ticketList = tickets.map((ticket) => (
+        <TicketCard key={ticket.id} {...ticket} />
+    ))
+    return (
+        <div className="w-full max-w-lg py-5 grid grid-cols-4 gap-4">
+            {ticketList}
+        </div>
+    )
 }
 
 LandingPage.getInitialProps = async (context, client) => {
