489dc6e85a197ffa72ac72299bb6f8052a891126
1sh1vam <shivam07.mt@gmail.com>
2022-05-07 17:16:27 +0530

    adding the link to view the ticket

--- client/components/ticket-card.js
+++ client/components/ticket-card.js
@@ -1,8 +1,19 @@
-const TicketCard = ({ title, price }) => {
+import Link from "next/link";
+
+const TicketCard = ({ title, price, id }) => {
     return (
-        <div className="w-full p-5 flex items-start rounded-md justify-between text-md font-md shadow-lg bg-gray-100 text-gray-800 gap-5">
-            <p className="m-0">{title}</p>
-            <p className="m-0">${price}</p>
+        <div className="w-full p-5 flex flex-col items-start rounded-md justify-between text-md font-md shadow-lg bg-gray-100 text-gray-800 gap-2">
+            <div>
+                <p className="text-xs text-indigo-500">Title</p>
+                <p className="m-0">{title}</p>
+            </div>
+            <div>
+                <p className="text-xs text-indigo-500">Price</p>
+                <p className="m-0">${price}</p>
+            </div>
+            <Link href='/tickets/[ticketId]' as={`/tickets/${id}`}>
+                <a className="decoration-0 mt-2 w-full tracking-wider text-center py-1 bg-indigo-500 text-white rounded-md last-of-type:mr-0 hover:scale-110">view</a>
+            </Link>
         </div>
     )
 }
