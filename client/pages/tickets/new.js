72b6d6f1ab1f6b04c543ba87492f11df82dca855
1sh1vam <shivam07.mt@gmail.com>
2022-05-07 14:13:11 +0530

    handling ticket inputs

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,17 +1,29 @@
+import { useState } from "react";
+
 const NewTicket = () => {
+    const [title, setTitle] = useState('');
+    const [price, setPrice] = useState('');
+
+    const handlePriceChange = (e) => {
+        const value = parseFloat(e.target.value);
+        setPrice((prev) => !isNaN(value) ? value : prev);
+    }
     return (
         <div className="w-full translate-y-40 flex justify-center items-center">
             <div className="w-full max-w-md rounded-lg shadow-md bg-gray-150 flex font-sm text-sm text-gray-800 flex-col gap-6 p-8">
                 <div className="w-full">
                     <p className="text-left">Title</p>
                     <input
+                        value={title}
+                        onChange={({ target }) => setTitle(target.value)}
                         className="w-full border mt-1 rounded-md px-3 h-10 border-gray-300 outline-none focus:outline-none focus:border-blue-300"
                     />
                 </div>
                 <div className="w-full">
                     <p className="text-left">Price</p>
                     <input
-                        type="number"
+                        value={price}
+                        onChange={handlePriceChange}
                         className="w-full border mt-1 rounded-md px-3 h-10 border-gray-300 outline-none focus:outline-none focus:border-blue-300"
                     />
                 </div>
