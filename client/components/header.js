336fe12515dbf7f427589e1cd090aa32b27aa47f
1sh1vam <shivam07.mt@gmail.com>
2022-05-07 19:15:49 +0530

    setting up the sell ticket and my orders in header

--- client/components/header.js
+++ client/components/header.js
@@ -2,19 +2,23 @@ import Link from 'next/link';
 
 export default ({ currentUser }) => {
     const links = [
+        currentUser && { href: '/tickets/new', label: 'Sell Tickets' },
+        currentUser && { href: '/orders', label: 'My Orders' },
         !currentUser && {href: '/auth/signup', label: 'Sign up'},
         !currentUser && {href: '/auth/signin', label: 'Sign in'},
         currentUser && {href: '/auth/signout', label: 'Sign out'},
     ].filter((link) => link)
     .map(({ href, label }) => (
         <Link href={href} key={href}>
-            <a className="mr-2 decoration-0 px-3 py-1 border border-indigo-500 rounded-md last-of-type:mr-0">{label}</a>
+            <a className="mr-4 text-sm text-blue-400 decoration-0 py-1 borde rounded-md last-of-type:mr-0">{label}</a>
         </Link>
     ))
 
     return (
         <div className="box-border w-full flex mb-5 justify-between items-center">
-            <h3 className="text-gray text-xl border px-2 py-1 rounded-md">YTick</h3>
+            <Link href="/">
+                <a className="text-gray decoration-0 text-xl border px-2 py-1 rounded-md">YTick</a>
+            </Link>
             <div className="flex items-center px-5">
                 {links}
             </div>
